3e2aed1e88e44f1bdcb9822a5d3020044b376005
duongtnhat <duongtnhat@gmail.com>
2018-09-27 08:32:54 +0700

    Chatwork Notifier

---
+++ chatwork.js
@@ -0,0 +1,29 @@
+var request = require('request');
+
+var apiKey = '2b642c7d187eb153e47e7509d7f69356'
+
+setInterval(function() {
+request({
+      headers: {
+        'X-ChatWorkToken': apiKey
+      },
+      uri: 'https://api.chatwork.com/v2/my/status'
+    }, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+	var json = JSON.parse(body);
+        console.log(new Date(), 'Unread_room_num:', json.unread_room_num)
+	if (json.unread_room_num > 0) {
+	  request({
+            headers: {'X-ChatWorkToken': apiKey},
+            uri: 'https://api.chatwork.com/v2/rooms'
+            }, function (error, response, body) {
+		var json = JSON.parse(body);
+		json.forEach(function(room){
+		  if (room.unread_num > 0) console.log("\tRoom:", room.name, " https://www.chatwork.com/#!rid"+room.room_id);
+		});
+            })
+	}
+     }
+});
+
+}, 60000);
